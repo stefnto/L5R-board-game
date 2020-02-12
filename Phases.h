6b5df0d91d45a6c2717162f6a6dbdb20da2a1f9e
tolaras-gamias <60941536+tolaras-gamias@users.noreply.github.com>
2020-02-12 15:55:50 +0200

    Update and rename README.md to Phases.h

---
+++ Phases.h
@@ -0,0 +1,34 @@
+# Cpp-Project
+
+ Added files
+class startingPhase{
+  public:
+    void untapEverything();
+    void drawFateCard();
+    void revealProvinces();
+    void printHand();
+    void printProvinces();
+};
+
+class equipPhase{
+  public:
+
+};
+
+class battlePhase{
+
+};
+
+class economyPhase{
+
+};
+
+class finalPhase{
+  public:
+    void discardSurplusFateCards();
+    void printGameStatistics();
+    void printHand();
+    void printProvinces();
+    void printArena();
+    void printHoldings();
+};
--- README.md
+++
@@ -1,3 +0,0 @@
-# Cpp-Project
-
- Added files
