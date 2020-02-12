7c9b799573680c05df1d442fd8392fc539d160a6
tolaras-gamias <60941536+tolaras-gamias@users.noreply.github.com>
2020-02-12 15:58:23 +0200

    Update Phases.h

--- Phases.h
+++ Phases.h
@@ -1,34 +1,3 @@
 # Cpp-Project
 
  Added files
-class startingPhase{
-  public:
-    void untapEverything();
-    void drawFateCard();
-    void revealProvinces();
-    void printHand();
-    void printProvinces();
-};
-
-class equipPhase{
-  public:
-
-};
-
-class battlePhase{
-
-};
-
-class economyPhase{
-
-};
-
-class finalPhase{
-  public:
-    void discardSurplusFateCards();
-    void printGameStatistics();
-    void printHand();
-    void printProvinces();
-    void printArena();
-    void printHoldings();
-};
