ee8e333ef47ff76e93ee055c4b5d56b1a46f48bf
tolaras-gamias <60941536+tolaras-gamias@users.noreply.github.com>
2020-02-12 15:59:36 +0200

    Create Phases.h

---
+++ Phases.h
@@ -0,0 +1,31 @@
+class startingPhase{
+  public:
+    void untapEverything();
+    void drawFateCard();
+    void revealProvinces();
+    void printHand();
+    void printProvinces();
+};
+
+class equipPhase{
+  public:
+
+};
+
+class battlePhase{
+
+};
+
+class economyPhase{
+
+};
+
+class finalPhase{
+  public:
+    void discardSurplusFateCards();
+    void printGameStatistics();
+    void printHand();
+    void printProvinces();
+    void printArena();
+    void printHoldings();
+};
