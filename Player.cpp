0fd662f70fee14922c89090c7cf746d21320fe30
tolaras-gamias <60941536+tolaras-gamias@users.noreply.github.com>
2020-02-13 18:44:11 +0200

    Create Player.cpp

---
+++ Player.cpp
@@ -0,0 +1,15 @@
+#include "Player.h"
+
+Player::Player(){
+  cout << "Player created\n";
+  numberOfProvinces = 4;
+  deck = new DeckBuilder();
+  deck.createFateDeck();
+  deck.createDynastyDeck();    //θα χρειαστει επισης να χρησιμοποιησουμε τις συναρτησεις shuffle μεσα στις create
+  //επειδη εχουν σαν ορισμα τα private μελη του deckbuilder
+}
+
+Player::~Player(){
+  cout << "Player destroyed\n";
+  delete deck;
+}
