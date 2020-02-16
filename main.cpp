1c9a13f908016fef056f573660e8cfc93fb7d055
tolaras-gamias <60941536+tolaras-gamias@users.noreply.github.com>
2020-02-16 18:56:46 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include "Card.hpp"
+#include "DeckBuilder.hpp"
+#include "Follower.hpp"
+#include "GameBoard.hpp"
+#include "Holding.hpp"
+#include "Item.hpp"
+#include "Personality.hpp"
+#include "Player.hpp"
+// #include "TypeConverter.hpp"
+
+using namespace std;
+
+int main(void){
+  GameBoard game(3);
+}
---
+++ makefile
@@ -0,0 +1,9 @@
+objects=main.o Card.o DeckBuilder.o Follower.o GameBoard.o Holding.o Item.o Personality.o Player.o
+
+askisi1 : $(objects)
+	g++ $(objects) -o project
+
+main.o : Card.hpp DeckBuilder.hpp Follower.hpp GameBoard.hpp Holding.hpp Item.hpp Personality.hpp Player.hpp
+
+clean :
+	rm project $(objects)
